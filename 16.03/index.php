9b93bdf677a12fb4037ac2cc6196800c9ddd87af
bakedbobb <bakedbobb@gmail.com>
2026-03-16 10:59:57 +0100

    lekcja1

---
+++ 16.03/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php                   // <? sie nie używa         http://localhost/MateuszBednarczyk/16.03/
+
+    # php jest server side
+    # php to jezyk skryptowy - nie kompiluje sie
+    # php wysyla kod z serwera do urzytkownika
+
+
+    # ; należy dawać na koniec każdej linii
+
+    
+    echo "Hello world!";
+    
+    ?>
+</body>
+</html>
